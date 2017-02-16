5681956fc0bad6a6d11033c8d3e9eab78bf87d38
André Davis <CubicleJockey@users.noreply.github.com>
2017-02-15 21:03:16 -0800

    Starting to look into NUnit Attributes

---
+++ NUnit/NUnitObjects.UnitTests/Attributes/RepeatTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitObjects.UnitTests.Attributes
+{
+    [TestFixture]
+    public class RepeatTests
+    {
+        private const int REPEAT = 5;
+        private static int count = 0;
+
+        [Test]
+        [Repeat(REPEAT)]
+        public void RepeatedTests_Success()
+        {
+            Console.WriteLine($"Success run number {++count}");
+        }
+
+        [Test]
+        [Repeat(REPEAT)]
+        public void RepeatedTests_Stop()
+        {
+            const int FAILON = 3;
+            //test run increment
+            count = ++count;
+
+            if(count == FAILON)
+            {
+                Assert.Fail($"Repeat Failure, no tests past {FAILON}");
+            }
+            Console.WriteLine($"Success run number {count}");
+        }
+    }
+}
