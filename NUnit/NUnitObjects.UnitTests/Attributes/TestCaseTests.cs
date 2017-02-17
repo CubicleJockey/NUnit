28d39413547452a4b8ea24d44b87f007fa928107
André Davis <CubicleJockey@users.noreply.github.com>
2017-02-16 16:43:46 -0800

    More Testing Woot.
    On a MAC in Visual Studio.

---
+++ NUnit/NUnitObjects.UnitTests/Attributes/TestCaseTests.cs
@@ -0,0 +1,59 @@
+﻿using static System.Console;
+using NUnit.Framework;
+using NUnitObjects.Models;
+
+namespace NUnitObjects.UnitTests.Attributes
+{
+	/// <summary>
+	/// TestCaseAttribute serves the dual purpose of marking a method with parameters as a test method and 
+	/// providing inline data to be used when invoking that method. Here is an example of a test being run three times, 
+	/// with three different sets of data
+	/// </summary>
+	[TestFixture]
+	public class TestCaseTests
+	{
+		[TestCase(12, 3, 4)]
+		[TestCase(12, 2, 6)]
+		[TestCase(12, 4, 3)]
+		public void DivideTests(int numerator, int denominator, int quotient)
+		{
+			Assert.AreEqual(quotient, numerator / denominator);
+		}
+
+		[TestCase(12, 3, ExpectedResult = 4)]
+		[TestCase(12, 2, ExpectedResult = 6)]
+		[TestCase(12, 4, ExpectedResult = 3)]
+		public int DivideTestSimplified(int numerator, int demoniator)
+		{
+			return (numerator / demoniator);
+		}
+
+		[TestCase(IgnoreReason = "Because I hate this test.")]
+		public void IgnoreAndReasonTests()
+		{
+			//Ignored
+		}
+
+		[TestCase(IncludePlatform = "Win95, Win98, WinME, Win7, Win10, Net-2.0, Unix, Linux")]
+		public void IncludeThesePlatforms()
+		{
+			//platforms this is valid on
+		}
+
+		[TestCase(ExcludePlatform = "Mono, Mono-1.0, Mono-2.0")]
+		public void ExcludeThesePlatforms()
+		{
+			//Excluded Platforms
+		}
+
+		/// <summary>
+		/// You can currently only have one TestOf attribute per fixture or test.
+		/// </summary>
+		[TestCase(TestOf = typeof(Account))]
+		public void TestOf()
+		{
+			//This test is only for Account
+			WriteLine($"This test is set to only Test {nameof(Account)}");
+		}
+	}
+}
--- NUnit/NUnitObjects.UnitTests/Attributes/TheoryTests.cs
+++ NUnit/NUnitObjects.UnitTests/Attributes/TheoryTests.cs
@@ -1,10 +1,42 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace NUnitObjects.UnitTests.Attributes
 {
+	/// <summary>
+	/// A Theory is a special type of test, used to verify a general statement about the system under development.
+	/// Normal tests are example-based. That is, the developer supplies one or more examples of inputs and expected 
+	/// outputs either within the code of the test or - in the case of Parameterized Tests - as arguments to the 
+	/// test method. A theory, on the other hand, makes a general statement that all of its assertions will pass 
+	/// for all arguments satisfying certain assumptions.
+	/// </summary>
     [TestFixture]
     public class TheoryTests
     {
+		[DatapointSource]
+		public IEnumerable<double> dataPoints;
 
+		[SetUp]
+		public void SetUp()
+		{
+			dataPoints = new[] { 0.0, 1.0, -1.0, 42.0 };
+		}
+
+		/// <summary>
+		/// Given a non-negative number, the square root of that number is always non-negative and, 
+		/// when multiplied by itself, gives the original number.
+		/// </summary>
+		/// <param name="number">Number.</param>
+		[Theory]
+		public void SquareRootsDefinition(double number)
+		{
+			Assume.That(number >= 0.0);
+
+			var square = Math.Sqrt(number);
+
+			Assert.That(square >= 0.0);
+			Assert.That(square * square, Is.EqualTo(number).Within(0.00001));
+		}
     }
 }
